4134c731fc9951f4cdc7b545dc9b9bf648c6271e
Subrat <subratpalhar92@gmail.com>
2019-03-01 21:17:03 +0530

    Commited First Java Code & added some technial note

---
+++ JavaPractice/A1HelloWorld.java
@@ -0,0 +1,11 @@
+class A1HelloWorld {
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello World !!! This Is My First Step In To The Java World");
+    }
+
+}
