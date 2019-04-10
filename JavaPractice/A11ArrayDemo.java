5cc786ac83585180c07a8f1b87829d0b18643857
subratpalhar92 <subratpalhar92@gmail.com>
2019-04-11 00:32:32 +0530

    Resumed Practice :) - Added array declaration And manipulation

---
+++ JavaPractice/A11ArrayDemo.java
@@ -0,0 +1,9 @@
+class A11ArrayDemo{
+    public static void main(String args[]){
+        int MyFirstarray[] = new int[10];
+        double MySecondArray[] = {3.5, 6.7, 7.8, 8.88};
+
+        System.out.println("The Length Of My First Array Is : "+MyFirstarray.length);
+        System.out.println("The Length Of My Second Array Is : "+MySecondArray.length);
+    }
+}
